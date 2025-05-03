d89d5a422ddc3b86505f85a030a9bfa3c1191008
Juan Miguel <juan.miguel.palacios@correounivalle.edu.co>
2025-05-03 01:34:54 -0500

    refactor: cambio en la implementación de fuerza bruta en el problema palindrome

--- src/palindrome/brute_force_solution.py
+++ src/palindrome/brute_force_solution.py
@@ -1,35 +1,15 @@
-from itertools import combinations
-from typing import Generator
-import math
-
 from utils.is_palindrome import is_palindrome
-
-
-def calculate_max_length(n: int, max_combinations: int = 50000000000) -> int:
-    for k in range(1, n):
-        c = math.comb(n, k)
-
-        if c >= max_combinations:
-            return k
-
-    return 5
-
-
-def all_subsequences(text: str) -> Generator[str, None, None]:
-    text_length: int = len(text)
-    max_length: int = min(text_length, calculate_max_length(text_length))
-
-    for length in range(max_length, 0, -1):
-        for text_index in combinations(range(text_length), length):
-            yield "".join(text[i] for i in text_index)
 
 
 def max_subsequence_palindrome(text: str) -> str:
     longest_palindrome: str = ""
+    text_length: int = len(text)
 
-    for subseq in all_subsequences(text):
-        if is_palindrome(subseq):
-            longest_palindrome = subseq
-            return longest_palindrome
+    for start in range(text_length):
+        for end in range(start + 1, text_length + 1):
+            substring: str = text[start:end]
+
+            if is_palindrome(substring) and len(substring) > len(longest_palindrome):
+                longest_palindrome = substring
 
     return longest_palindrome
