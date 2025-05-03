98614bcf9e34ea474b845970dfb20aefd21899e6
Juan Miguel <juan.miguel.palacios@correounivalle.edu.co>
2025-05-02 23:50:28 -0500

    fix: reseteo del main.py

--- src/main.py
+++
@@ -1,8 +0,0 @@
-from utils.normalize import normalizer
-from palindrome.brute_force_solution import max_subsequence_palindrome
-
-if __name__ == "__main__":
-    text = normalizer(
-        "Llego a tierra y le dijo: Dabale arroz a la zorra el abad, ella aceptó"
-    )
-    print(max_subsequence_palindrome("radaroso"))
