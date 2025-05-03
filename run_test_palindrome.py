a265db3c7abd921440b6d724fbb3d25096960403
JAlexis1 <ruizquicenojhonalexis@gmail.com>
2025-05-02 22:38:07 -0500

    run test desde raiz mas path en python

--- run_test_palindrome.py
+++ run_test_palindrome.py
@@ -5,7 +5,7 @@ import unittest
 sys.path.insert(0, os.path.abspath('src'))
 
 specific_test_folder = os.path.join('tests', 'palindrome')
-suite = unittest.defaultTestLoader.discover(specific_test_folder, pattern='test*.py')
+suite = unittest.defaultTestLoader.discover(specific_test_folder, pattern='test_*.py')
 
 if __name__ == "__main__":
     runner = unittest.TextTestRunner(verbosity=2)
