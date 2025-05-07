e2f26e9b69a561ea3fc425e8e46b8ff29744d626
Nicolás Rodriguez <117177426+NiKolas1828@users.noreply.github.com>
2025-05-06 23:48:32 -0500

    correccion test dinamica palindrome

--- tests/palindrome/test_dinamic_solution.py
+++ tests/palindrome/test_dinamic_solution.py
@@ -6,17 +6,6 @@ class TestMaxDynamicPalindrome(TestRepetition):
     def setUp(self):
         self.setAlgorithm(max_dynamic_palindrome)
 
-    def test_10(self):
-        self.run_n_repetitions(10, 1)
-
-    def test_100(self):
-        self.run_n_repetitions(100)
-
-    def test_1000(self):
-        self.run_n_repetitions(1000)
-
-    def test_10000(self):
-        self.run_n_repetitions(10000)
-
-    def test_50000(self):
-        self.run_n_repetitions(50000)
+    def test_dynamic_solution(self):
+        print("\nTiempos de ejecución de solucion dinamica palindrome")
+        self.run_all_tests()
