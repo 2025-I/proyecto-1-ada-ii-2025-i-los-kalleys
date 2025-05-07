e4c3990eae3885876c35b8473e74ab97275ae0c4
JAlexis1 <ruizquicenojhonalexis@gmail.com>
2025-05-06 23:20:55 -0500

    Refactorización del test para voraz palindrome

--- tests/palindrome/test_greedy_solution.py
+++ tests/palindrome/test_greedy_solution.py
@@ -1,4 +1,4 @@
-from palindrome.repetition import TestRepetition
+from tests.palindrome.repetition import TestRepetition
 from src.palindrome.greedy_solution import greedy_palindromic_substring
 
 
@@ -6,17 +6,6 @@ class TestGreedyPalindrome(TestRepetition):
     def setUp(self):
         self.setAlgorithm(greedy_palindromic_substring)
 
-    def test_10(self):
-        self.run_n_repetitions(10, 1)
-
-    def test_100(self):
-        self.run_n_repetitions(100)
-
-    def test_1000(self):
-        self.run_n_repetitions(1000)
-
-    def test_10000(self):
-        self.run_n_repetitions(10000)
-
-    def test_50000(self):
-        self.run_n_repetitions(50000)
+    def test_greedy_solution(self):
+        print("\nTiempos de ejecución de solucion voraz palindrome")
+        self.run_all_tests()
